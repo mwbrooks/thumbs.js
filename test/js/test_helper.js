0e28a1d27bd396f676a8622a0d001fd33db0409d
Michael Brooks <michael@michaelbrooks.ca>
2011-02-26 11:06:18 -0800

    Reindent test_helper.js to 4 spaces.

--- test/js/test_helper.js
+++ test/js/test_helper.js
@@ -1,123 +1,121 @@
 (function(scope) {
 
+    var TIMEOUT = 300;
+
+    /**
+     * QUnit Module Environment
+     */
     scope.environment = {
         setup: function() {
-            var el = document.createElement('a');
-            el.setAttribute('href', '#');
-            el.setAttribute('id', 'fixture');
-            document.getElementById('qunit-fixture').appendChild(el);
+            document.getElementById('qunit-fixture').innerHTML = '<a href="#" id="fixture"></a>';
         },
         teardown: function() {
             document.getElementById('qunit-fixture').innerHTML = '';
         }
     };
 
-
-
-  var LISTENER_TIMEOUT = 300;
-
-  /**
-   * Default Options for Listener
-   */
-  var defaultOptions = {
-    onTrigger: function(e) {
-      ok(true, 'received ' + name + ' event');
-    },
-    onTimeout: function() {
-      ok(false, 'Timed out');
-    }
-  };
-
-  /**
-   * Event Listener Assertion
-   *
-   * Listens for an event on a custom element and
-   * fires onTrigger or onTimeout.
-   *
-   * @param  {String} name of the event to listen for
-   * @param  {Object} options { onTrigger: {Function}, onTimeout: {Function} }
-   * @return {Object} of chainable actions
-   */
-  scope.listen = function(name, options) {
-    QUnit.stop();
-    expect(1);
-
-    var options = merge(options, defaultOptions);
-    var element = document.getElementById('fixture');
-    
-    element.addEventListener(name, function(e) {
-      clearTimeout(timeoutId);
-      options.onTrigger(e);
-      QUnit.start();
-    }, false);
-
-    var timeoutId = setTimeout(function() {
-      options.onTimeout();
-      QUnit.start();
-    }, LISTENER_TIMEOUT);
-
-    return {
-      /**
-       * Trigger an event on the listener's element
-       *
-       * @param {String} name of event to trigger
-       */
-      trigger: function(name) {
-        var e = createEvent(name);
-        var element = document.getElementById('fixture');
-        element.dispatchEvent(e);
-        return this;
-      }
+    /**
+     * Default Options for Listener
+     */
+    var defaultOptions = {
+        onTrigger: function(e) {
+            ok(true, 'received ' + name + ' event');
+        },
+        onTimeout: function() {
+            ok(false, 'Timed out');
+        }
     };
-  };
 
-  /**
-   * Merge Objects
-   *
-   * @param {Object} original with priority
-   * @param {Object} defaults for the obj
-   * @return {Object} new object
-   */
-  var merge = function(original, defaults) {
-    var obj = original || {};
+    /**
+     * Event Listener Assertion
+     *
+     * Listens for an event on a custom element and
+     * fires onTrigger or onTimeout.
+     *
+     * @param  {String} name of the event to listen for
+     * @param  {Object} options { onTrigger: {Function}, onTimeout: {Function} }
+     * @return {Object} of chainable actions
+     */
+    scope.listen = function(name, options) {
+        QUnit.stop();
+        expect(1);
 
-    for (var key in defaults) {
-      if (typeof obj[key] === 'undefined') {
-        obj[key] = defaults[key];
-      }
-    }
+        var options = merge(options, defaultOptions);
+        var element = document.getElementById('fixture');
 
-    return obj;
-  };
+        element.addEventListener(name, function(e) {
+            clearTimeout(timeoutId);
+            options.onTrigger(e);
+            QUnit.start();
+        }, false);
 
-  /**
-   * Utility function to create an event.
-   *
-   * @param name of the event
-   * @return event object
-   */
-  var createEvent = function(name, e) {
-    var event = document.createEvent('MouseEvents');
+        var timeoutId = setTimeout(function() {
+            options.onTimeout();
+            QUnit.start();
+        }, TIMEOUT);
 
-    event.initMouseEvent(
-      name,         // eventType
-      true,         // canBubble
-      true,         // cancelable
-      window,       // view
-      0,            // detail (mouse click count)
-      0,            // screenX
-      0,            // screenY
-      0,            // clientX
-      0,            // clientY
-      false,        // ctrlKey
-      false,        // altKey
-      false,        // shiftKey
-      false,        // metaKey
-      0,            // button
-      null          // relatedTarget
-    );
+        return {
+            /**
+             * Trigger an event on the listener's element
+             *
+             * @param {String} name of event to trigger
+             */
+            trigger: function(name) {
+                var e = createEvent(name);
+                var element = document.getElementById('fixture');
+                element.dispatchEvent(e);
+                return this;
+            }
+        };
+    };
 
-    return event;
-  };
+    /**
+     * Merge Objects
+     *
+     * @param {Object} original with priority
+     * @param {Object} defaults for the obj
+     * @return {Object} new object
+     */
+    var merge = function(original, defaults) {
+        var obj = original || {};
+
+        for (var key in defaults) {
+            if (typeof obj[key] === 'undefined') {
+                obj[key] = defaults[key];
+            }
+        }
+
+        return obj;
+    };
+
+    /**
+     * Utility function to create an event.
+     *
+     * @param name of the event
+     * @return event object
+     */
+    var createEvent = function(name, e) {
+        var event = document.createEvent('MouseEvents');
+
+        event.initMouseEvent(
+            name,         // eventType
+            true,         // canBubble
+            true,         // cancelable
+            window,       // view
+            0,            // detail (mouse click count)
+            0,            // screenX
+            0,            // screenY
+            0,            // clientX
+            0,            // clientY
+            false,        // ctrlKey
+            false,        // altKey
+            false,        // shiftKey
+            false,        // metaKey
+            0,            // button
+            null          // relatedTarget
+        );
+
+        return event;
+    };
 
 })(window);
